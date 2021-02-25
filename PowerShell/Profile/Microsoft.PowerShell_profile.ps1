a94d8d5cda030b3e54b090137925d056f7636fd8
progCM44 <progcm44@gmail.com>
2021-02-25 19:24:56 +0100

    Move + profile

---
+++ PowerShell/Profile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+write-host "Bienvenue Maxime"
+write-host "		"
+write-host "Liste des fonctions definies:"
+write-host "	- docker-start-full"
+write-host "	- docker-build-full"
+write-host "	- docker-stop-full"
+
+function docker-start-full
+{
+            C:\Users\33685\Desktop\Programmation\Repository\Scripts\startPosteDevLocal.ps1
+}
+
+function docker-build-full
+{
+            C:\Users\33685\Desktop\Programmation\Repository\Scripts\buildImages.ps1
+}
+
+function docker-stop-full
+{
+            C:\Users\33685\Desktop\Programmation\Repository\Scripts\stopPosteDevLocal.ps1
+}
