9896d1d0de6da516b1da5edafaa1c2984f965019
progCM44 <progcm44@gmail.com>
2021-02-25 19:22:39 +0100

    Ajout scripts PosteLocalDocker

---
+++ buildImages.ps1
@@ -0,0 +1,3 @@
+#BUILD CONTAINER CACE
+cd C:\Users\33685\Desktop\Programmation\Repository\CACE
+docker build -t cace:latest .
---
+++ stopPosteDevLocal.ps1
@@ -0,0 +1,6 @@
+docker stop cace
+docker rm cace
+docker stop cape
+docker rm cape
+docker stop portainer
+docker rm portainer
