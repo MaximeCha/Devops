5f13619d32695a25df2b80826335fbac69b5f102
progCM44 <progcm44@gmail.com>
2021-02-25 19:46:12 +0100

    Correction path

--- PowerShell/Profile/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Profile/Microsoft.PowerShell_profile.ps1
@@ -7,15 +7,15 @@ write-host "	- docker-stop-full"
 
 function docker-start-full
 {
-            C:\Users\33685\Desktop\Programmation\Repository\Scripts\startPosteDevLocal.ps1
+            C:\Users\33685\Desktop\Programmation\Repository\Devops\PowerShell\Scripts\startPosteDevLocal.ps1
 }
 
 function docker-build-full
 {
-            C:\Users\33685\Desktop\Programmation\Repository\Scripts\buildImages.ps1
+            C:\Users\33685\Desktop\Programmation\Repository\Devops\PowerShell\Scripts\buildImages.ps1
 }
 
 function docker-stop-full
 {
-            C:\Users\33685\Desktop\Programmation\Repository\Scripts\stopPosteDevLocal.ps1
+            C:\Users\33685\Desktop\Programmation\Repository\Devops\PowerShell\Scripts\stopPosteDevLocal.ps1
 }
